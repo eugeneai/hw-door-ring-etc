574a97b3366d5a86b689579ae2e097bd9906fd91
Evgeny Cherkashin <eugeneai@irnok.net>
2019-04-21 12:10:16 +0800

    Added ASCII Character set.

--- code/doorbell.c
+++ code/doorbell.c
@@ -14,6 +14,7 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <string.h>
+#include <stdint.h>
 
 #define LED 5
 #define BUSY 0
@@ -33,6 +34,114 @@ int LED7test(void);
 /* { */
 /*     PORTB.0=1; */
 /* } */
+
+#define _DP 0x80
+#define _G  0x01
+#define _F  0x02
+#define _E  0x04
+#define _D  0x08
+#define _C  0x10
+#define _B  0x20
+#define _A  0x40
+
+const uint8_t SevenSegmentASCII[96] = {
+	 0                              , /* (space) */
+	 _DP                |_C |_B     , /* ! */
+	         _F             |_B     , /* " */
+	     _G |_F |_E |_D |_C |_B     , /* # */
+	     _G |_F     |_D |_C     |_A , /* $ */
+	 _DP|_G     |_E         |_B     , /* % */
+	     _G             |_C |_B     , /* & */
+	         _F                     , /* ' */
+	         _F     |_D         |_A , /* ( */
+	                 _D     |_B |_A , /* ) */
+	         _F                 |_A , /* * */
+	     _G |_F |_E                 , /* + */
+	             _E                 , /* , */
+	     _G                         , /* - */
+	 _DP                            , /* . */
+	     _G     |_E         |_B     , /* / */
+	         _F |_E |_D |_C |_B |_A , /* 0 */
+	                     _C |_B     , /* 1 */
+	     _G     |_E |_D     |_B |_A , /* 2 */
+	     _G         |_D |_C |_B |_A , /* 3 */
+	     _G |_F         |_C |_B     , /* 4 */
+	     _G |_F     |_D |_C     |_A , /* 5 */
+	     _G |_F |_E |_D |_C     |_A , /* 6 */
+	                     _C |_B |_A , /* 7 */
+	     _G |_F |_E |_D |_C |_B |_A , /* 8 */
+	     _G |_F     |_D |_C |_B |_A , /* 9 */
+	                 _D         |_A , /* : */
+	                 _D |_C     |_A , /* ; */
+	     _G |_F                 |_A , /* < */
+	     _G         |_D             , /* = */
+	     _G                 |_B |_A , /* > */
+	 _DP|_G     |_E         |_B |_A , /* ? */
+	     _G     |_E |_D |_C |_B |_A , /* @ */
+	     _G |_F |_E     |_C |_B |_A , /* A */
+	     _G |_F |_E |_D |_C         , /* B */
+	         _F |_E |_D         |_A , /* C */
+	     _G     |_E |_D |_C |_B     , /* D */
+	     _G |_F |_E |_D         |_A , /* E */
+	     _G |_F |_E             |_A , /* F */
+	         _F |_E |_D |_C     |_A , /* G */
+	     _G |_F |_E     |_C |_B     , /* H */
+	         _F |_E                 , /* I */
+	             _E |_D |_C |_B     , /* J */
+	     _G |_F |_E     |_C     |_A , /* K */
+	         _F |_E |_D             , /* L */
+	             _E     |_C     |_A , /* M */
+	         _F |_E     |_C |_B |_A , /* N */
+	         _F |_E |_D |_C |_B |_A , /* O */
+	     _G |_F |_E         |_B |_A , /* P */
+	     _G |_F     |_D     |_B |_A , /* Q */
+	         _F |_E         |_B |_A , /* R */
+	     _G |_F     |_D |_C     |_A , /* S */
+	     _G |_F |_E |_D             , /* T */
+	         _F |_E |_D |_C |_B     , /* U */
+	         _F |_E |_D |_C |_B     , /* V */
+	         _F     |_D     |_B     , /* W */
+	     _G |_F |_E     |_C |_B     , /* X */
+	     _G |_F     |_D |_C |_B     , /* Y */
+	     _G     |_E |_D     |_B |_A , /* Z */
+	         _F |_E |_D         |_A , /* [ */
+	     _G |_F         |_C         , /* \ */
+	                 _D |_C |_B |_A , /* ] */
+	         _F             |_B |_A , /* ^ */
+	                 _D             , /* _ */
+	                         _B     , /* ` */
+	     _G     |_E |_D |_C |_B |_A , /* a */
+	     _G |_F |_E |_D |_C         , /* b */
+	     _G     |_E |_D             , /* c */
+	     _G     |_E |_D |_C |_B     , /* d */
+	     _G |_F |_E |_D     |_B |_A , /* e */
+	     _G |_F |_E             |_A , /* f */
+	     _G |_F     |_D |_C |_B |_A , /* g */
+	     _G |_F |_E     |_C         , /* h */
+	             _E                 , /* i */
+	                 _D |_C         , /* j */
+	     _G |_F |_E     |_C     |_A , /* k */
+	         _F |_E                 , /* l */
+	             _E     |_C         , /* m */
+	     _G     |_E     |_C         , /* n */
+	     _G     |_E |_D |_C         , /* o */
+	     _G |_F |_E         |_B |_A , /* p */
+	     _G |_F         |_C |_B |_A , /* q */
+	     _G     |_E                 , /* r */
+	     _G |_F     |_D |_C     |_A , /* s */
+	     _G |_F |_E |_D             , /* t */
+	             _E |_D |_C         , /* u */
+	             _E |_D |_C         , /* v */
+	             _E     |_C         , /* w */
+	     _G |_F |_E     |_C |_B     , /* x */
+	     _G |_F     |_D |_C |_B     , /* y */
+	     _G     |_E |_D     |_B |_A , /* z */
+	     _G             |_C |_B     , /* { */
+	         _F |_E                 , /* | */
+	     _G |_F |_E                 , /* } */
+	                             _A , /* ~ */
+	 0                              , /* (del) */
+};
 
 
 int uart_putchar(char c, struct __file * unused) {
@@ -129,49 +238,6 @@ void timer_init() {
   sei();
 };
 
-#define STEPS 5000
-#define DELAY 1000
-
-int main() {
-
-  uart_init();
-  timer_init();
-
-  // stdout = &uart_output;
-  // stdin  = &uart_input;
-
-  // TEST
-  //DDRL  = 0x00;
-  //PORTL = 0x00;
-  //DDRD |= 1<<6;
-
-  DDRB |= 1<<LED;
-  DDRB &= ~(1<<BUSY);
-  PORTB &= ~(1<<LED);
-
-
-  // unsigned long int pp = 0;
-
-  // _delay_ms(DELAY);
-
-  // printf("Starting:\n");
-
-  for (;;)  {
-    /* pulses.p.low = get_part(); */
-    /* // printf("Count: o:%u %lu (+%lu / sec) time: %u ", pulses.p.high, pulses.all, (pulses.all-pp), ticks); */
-    /* printf("Count: o:%u %lu (pp =%lu) time: %u ", pulses.p.high, pulses.all, pp, ticks); */
-    /* printf("\n"); */
-    /* pp=pulses.all; */
-    /* _delay_ms(DELAY); */
-    if (! playing)
-      LED7test();
-
-    sleep_mode();
-  }
-  return 0;
-}
-
-
 // Taken from https://gist.githubusercontent.com/adnbr/2352797/raw/b5e33d85167e6b562a6b4720921a2955eb3cc631/max7219-basic.c
 
 /* MAX7219 Interaction Code
@@ -249,12 +315,10 @@ void MAX7219_clearDisplay()
 
 void MAX7219_clearDisplay0()
 {
-    char i = digitsInUse;
-    // Loop until 0, but don't run for zero
-    do {
-        // Set each display in use to blank
-        MAX7219_writeData(i, 0);
-    } while (--i);
+  for(char i; i<8; i++) {
+    D[i]=0;
+    MAX7219_writeData(i, 0);
+  };
 }
 
 void MAX7219_displayDigits(unsigned int num) {
@@ -303,8 +367,7 @@ void MAX7219_displayNumber(volatile long number)
     }
 }
 
-int LED7test(void)
-{
+void LED7_init() {
     // SCK MOSI CS/LOAD/SS
     DDRB |= (1 << PIN_SCK) | (1 << PIN_MOSI) | (1 << PIN_SS);
 
@@ -321,28 +384,126 @@ int LED7test(void)
     // Scan limit runs from 0.
     MAX7219_writeData(MAX7219_MODE_SCAN_LIMIT, digitsInUse - 1);
     MAX7219_writeData(MAX7219_MODE_INTENSITY, intensity);
+}
 
-    /* int i = 99999999; */
-    /* while(1) */
-    /* { */
-    /*     MAX7219_displayNumber(--i); */
-    /*     // _delay_ms(10); */
+void LED7_done() {
+  SPCR &= ~(1 << SPE);
+}
 
-    /*     if (i == 0) { */
-    /*       //i = 999; */
-    /*       break; */
-    /*     } */
-    /* } */
-    for (int i = 0; i<10; i++) {
-      _delay_ms(30);
-      MAX7219_displayDigits(digitsInUse);
-      int i;
-      for (i=0; i<digitsInUse-1; i++) {
-        D[i]=D[i+1];
-      }
-      // D[digitsInUse-1]++;
-      D[digitsInUse-1] = ~(D[0] ^ D[5]);
+int LED7test(void)
+{
+  LED7_init();
+  for (int i = 0; i<10; i++) {
+    _delay_ms(30);
+    MAX7219_displayDigits(digitsInUse);
+    int i;
+    for (i=0; i<digitsInUse-1; i++) {
+      D[i]=D[i+1];
     }
-    intensity++;
-    SPCR &= ~(1 << SPE);
+    // D[digitsInUse-1]++;
+    D[digitsInUse-1] = ~(D[0] ^ D[5]);
+  }
+  intensity++;
+  LED7_done();
+}
+
+
+int LED7_writeChar(const char c, uint8_t digit) {
+  char c1 = SevenSegmentASCII[c];
+  D[digit] = (uint8_t) c1;
+  MAX7219_displayDigits(digitsInUse);
+  return 0;
+}
+
+uint8_t LED7_POS = 0;
+int LED7_putchar(const char c, struct __file * unused) {
+  // c &=0x7f; // Remove 7-th bit;
+  char c1 = c - 32;
+  if (LED7_POS>=digitsInUse) LED7_POS=0;
+  LED7_writeChar(c1, LED7_POS++);
+  // loop_until_bit_is_set(UCSRA, TXC); /* Wait until transmission ready. */
+  return 0;
+}
+
+FILE LED7_output = FDEV_SETUP_STREAM(LED7_putchar, NULL, _FDEV_SETUP_WRITE);
+
+
+void LED7_stdioTest(void) {
+  LED7_init();
+  MAX7219_clearDisplay0();
+  printf("test5678");
+  char c=0;
+  /* while(1) { */
+  /*   for (char d=0; d<8; d++) { */
+  /*     LED7_writeChar(c, d); */
+  /*   } */
+  /*   c++; */
+  /*   _delay_ms(300); */
+  /* } */
+  LED7_done();
+}
+
+
+void LED7_s_test() {
+  LED7_init();
+  char c=1;
+  while(1) {
+    for (char d=0; d<8; d++) {
+      MAX7219_writeData(MAX7219_DIGIT0+d, c);
+      c<<=1;
+      if (c==0) c=1;
+    }
+    _delay_ms(2000);
+  }
+  LED7_done();
+}
+
+
+#define STEPS 5000
+#define DELAY 1000
+
+int main() {
+
+  uart_init();
+  timer_init();
+
+  // stdout = &uart_output;
+  stdout = &LED7_output;
+
+
+  // stdout = &uart_output;
+  // stdin  = &uart_input;
+
+  // TEST
+  //DDRL  = 0x00;
+  //PORTL = 0x00;
+  //DDRD |= 1<<6;
+
+  DDRB |= 1<<LED;
+  DDRB &= ~(1<<BUSY);
+  PORTB &= ~(1<<LED);
+
+
+  // unsigned long int pp = 0;
+
+  LED7_stdioTest();
+  // LED7_s_test();
+
+  _delay_ms(5000);
+
+  // printf("Starting:\n");
+
+  for (;;)  {
+    /* pulses.p.low = get_part(); */
+    /* // printf("Count: o:%u %lu (+%lu / sec) time: %u ", pulses.p.high, pulses.all, (pulses.all-pp), ticks); */
+    /* printf("Count: o:%u %lu (pp =%lu) time: %u ", pulses.p.high, pulses.all, pp, ticks); */
+    /* printf("\n"); */
+    /* pp=pulses.all; */
+    /* _delay_ms(DELAY); */
+    if (! playing)
+      // LED7test();
+
+    sleep_mode();
+  }
+  return 0;
 }
